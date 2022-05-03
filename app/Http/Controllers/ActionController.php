e3865272508a099a6d04d8dcad241f05725d6196
Yen <laichungyang@gmail.com>
2022-05-03 06:01:54 +0000

    p2初版

---
+++ app/Http/Controllers/ActionController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller as BaseController;
+use Illuminate\Support\Facades\Request;
+use App\Jobs\Getkey;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Queue\Worker;
+use Illuminate\Queue\Jobs\DatabaseJob;
+use App\Models\ApiKey;
+use Exception;
+
+class ActionController extends BaseController
+{
+
+    public function index()
+    {
+        try {
+            dump(Carbon::now()->format("H:i:s"));
+            // 第三方假資料
+            $third_domain = 'https://qwerty.third';
+            $path = Request::path();
+            $third_request = $third_domain . $path;
+            // 開始取得queue pop job
+            $job = Queue::connection('database')->pop('choose-api-key');
+            if (!$job) {
+                throw new Exception("queue裡沒有job ,無法使用", 1);
+            }
+            // job 解析
+            $payload = $job->payload();
+            $command = unserialize($payload['data']['command']);
+            $api_key = $command->key;
+            $api_num = $command->num;
+            // 執行job (1秒後重新排隊)
+            $job->fire();
+            $third_request .= http_build_query(['key' => $api_key, 'num' => $api_num]);
+            // 通過curl取得資料塞進data, 此處跳過實作
+            $data = [];
+            dump(Carbon::now()->format("H:i:s"));
+            return [
+                'status' => 0,
+                'msg' => 'url = ' . $third_request,
+                'data' => $data,
+            ];
+        } catch (\Throwable $th) {
+            dump(Carbon::now()->format("H:i:s"));
+            dd($th);
+            return [
+                'status' => 0,
+                'msg' => $th->getMessage()
+            ];
+        }
+    }
+
+    public function addJob(Request $request)
+    {
+        dump(Carbon::now()->format("H:i:s"));
+        $model = new ApiKey();
+        $apikeys = $model->all();
+        foreach ($apikeys as $apikey) {
+            $api_count = $apikey->count ?? 0;
+            for ($i = 0; $i < $api_count; $i++) {
+                $this->dispatch(new Getkey($apikey->api_key, $i + 1));
+            }
+        }
+
+        dump(Carbon::now()->format("H:i:s"));
+        // dd($a);
+        return 'END';
+    }
+}
---
+++ app/Jobs/Getkey.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+
+
+class Getkey implements ShouldQueue, ShouldBeUnique
+{
+    /**
+     * queue使用類型
+     */
+    public $connection = 'database';
+
+    /**
+     * queue列名稱
+     */
+    public $queue = 'choose-api-key';
+
+    /**
+     * 第三方apikey
+     */
+    public $key;
+
+    /**
+     * 1秒可用次數
+     */
+    public $num;
+
+    use Dispatchable, InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($key, $num)
+    {
+        $this->key = $key;
+        $this->num = $num;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // sleep(10);
+        // api_key 限制1秒3次 所以將key拆分成3個請求數 當執行完畢後過1秒重新排隊
+        $this->release(1);
+    }
+}
